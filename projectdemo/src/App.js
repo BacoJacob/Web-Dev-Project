9c67c2a094c9637da74a209f5f5ee6ee69efc928
Adrel410 <ogundipe_adeola@columbusstate.edu>
2022-11-07 19:53:00 +0000

    Basic App works

--- projectdemo/src/App.js
+++ projectdemo/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Button from 'react-bootstrap/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, React!</h1>
+        <Button variant="primary" size="lg" active>
+        Google Map API
+      </Button>
+      <Button variant="danger" onClick={() => { localStorage.setItem('theme', localStorage.getItem('theme') === 'dark' ? 'light' : 'dark'); window.location.reload();}}> Change Mode </Button>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
--- projectdemo/src/index.js
+++ projectdemo/src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Spinner from 'react-bootstrap/Spinner';
+import './sass/App.scss';
+
+const LightTheme = React.lazy(() => import('./components/LightTheme'));
+const DarkTheme = React.lazy(() => import('./components/DarkTheme'));
+
+const ThemeSelector = ({children}) => {  
+  const theme = localStorage.getItem('theme') ? localStorage.getItem('theme') : window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light";
+  localStorage.setItem('theme', theme)
+  return(
+    <React.Suspense fallback={<Spinner animation="border" variant="primary"/>} key={theme}>
+      {theme === 'dark' && <DarkTheme />}
+      {theme === 'dark' && <LightTheme />}
+      {children}
+    </React.Suspense>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeSelector>
+      <App />
+    </ThemeSelector> 
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//ReactDOM.render(<App />, document.getElementById('root'))
