77a054625dce2fbf7f7bc8802f75213dc9cd96b3
Adrel410 <ogundipe_adeola@columbusstate.edu>
2022-11-08 00:47:22 +0000

    Deleted projectdemo

--- trash-busters/src/App.js
+++ trash-busters/src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
-import './App.css';
+import './sass/App.scss';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello, React!</h1>
+        <Button variant="primary" size="lg" active>
+        Google Map API
+        </Button>        
       </header>
     </div>
   );
