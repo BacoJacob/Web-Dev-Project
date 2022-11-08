7d7532244c433719b5f47598c4f5f505e5b04105
Adrel410 <ogundipe_adeola@columbusstate.edu>
2022-11-08 00:48:25 +0000

    delete

--- projectdemo/src/components/DarkTheme.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import '../sass/Dark.scss'
-
-const Theme = () => <></>
-
-export default Theme
--- projectdemo/src/components/LightTheme.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import '../sass/Light.scss'
-
-const Theme = () => <></>
-
-export default Theme
