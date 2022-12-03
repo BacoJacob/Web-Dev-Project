07147808b5808845339de8aefe3473fc54bfd6cb
austinProGit <97987573+austinProGit@users.noreply.github.com>
2022-12-03 14:44:33 -0500

    removed excess buttons

--- trash-busters/src/components/CatManager.jsx
+++ trash-busters/src/components/CatManager.jsx
@@ -48,8 +48,6 @@ function CatManager() {
             </div>
             <div className="mt-2 d-flex justify-content-center">
                 <Button className="mr-2" variant="primary" onClick={fetch_data}>Get Cat Photo</Button>
-                <Button><i class="bi bi-hand-thumbs-up-fill" onClick={send_data}></i></Button>{' '}
-                <Button variant="danger" onClick={fetch_data}><i class="bi bi-hand-thumbs-down-fill"></i></Button>{' '}
                 <SaveButton></SaveButton>
             </div>
             <br></br>
